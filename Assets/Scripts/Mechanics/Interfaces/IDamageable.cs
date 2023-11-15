785c2a5def79f9d11645efa7643a4172cbe98b53
voixdigitale <cristian@voixdigitale.com>
2023-11-15 16:52:34 +0100

    organization

--- Assets/Scripts/Mechanics/Interfaces/IDamageable.cs
+++ Assets/Scripts/Mechanics/Interfaces/IDamageable.cs
@@ -1,18 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IDamageable : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public interface IDamageable : IHitable {
+    void TakeDamage(int teamId, int damageAmount);
 }
--- Assets/Scripts/Mechanics/Interfaces/IGrabbable.cs
+++ Assets/Scripts/Mechanics/Interfaces/IGrabbable.cs
@@ -1,18 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class IGrabbable : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public interface IGrabbable {
+    void GetGrabbed(int teamId, GameObject hitSource);
 }
