44f87ce83f6611608e637d750febf04b21d2072c
voixdigitale <cristian@voixdigitale.com>
2023-11-15 17:22:35 +0100

    cinemachine setup

--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerView : MonoBehaviour
-{
+public class PlayerView : EntityView
+    {
     // Start is called before the first frame update
     void Start()
     {
